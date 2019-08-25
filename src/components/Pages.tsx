3872b675ea4c0489b8f1bcd7c2b8db7660f1cffd
Dan Choi <ygdanchoi@gmail.com>
2019-08-25 19:04:26 -0400

    Rename stuff

--- src/components/Pages.tsx
+++ src/components/Pages.tsx
@@ -3,7 +3,7 @@ import { Switch } from 'react-router-dom';
 import LoggedInRoute from '../routes/LoggedInRoute';
 import LoggedOutRoute from '../routes/LoggedOutRoute';
 import Auth from './auth/Auth';
-import Main from './home/Routine';
+import Main from './home/Home';
 
 const Pages = () => {
   return (
